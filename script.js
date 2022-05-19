efd6c776424c9244371e28a04c9fbc9e7a73d2a6
limasuu <szlima.93@gmail.com>
2022-05-19 11:04:34 -0300

    Apresentando a tabuada de adição

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const QUADRODIV= document.querySelector("#quadro");
+const ADICAOBUTTON= document.querySelector("#adicao");
+const SUBTRACAOBUTTON= document.querySelector("#subtracao");
+const MULTIPLICACAOBUTTON= document.querySelector("#multiplicacao");
+const DIVISAOBUTTON= document.querySelector("#divisao");
 
 const calcularTabuada= op => {
     
@@ -23,3 +28,28 @@ const calcularTabuada= op => {
         }        
     };
 };
+
+
+
+ADICAOBUTTON.addEventListener('click', () => {
+
+    const tabuadaAdicao= calcularTabuada("ADIÇÃO");
+
+    for(let i=1; i<=10; i++){
+
+        const tabuada_i= tabuadaAdicao(i)();
+
+        const novaDiv= document.createElement('div');
+        QUADRODIV.appendChild(novaDiv);
+        novaDiv.classList.add('tabuada-i');
+        
+
+        for(let conta of tabuada_i){
+            const novaLinha= document.createElement('div');
+            novaDiv.appendChild(novaLinha);
+            novaLinha.classList.add('linha-tabuada-i');
+
+            novaLinha.innerHTML= conta[0] + "= " + conta[1];
+        }
+    }
+});
